17863effa10e06b1bb02b94a2a80690c78b090c7
Jordan Miller <jordan.j.miller@live.com>
2019-10-04 08:49:47 -0700

    cards mvps done, dont like how i did it, refactoring

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,81 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+const cardContainer = document.querySelector('.cards-container');
+
+let javascriptArticles = [];
+let bootstrapArticles = [];
+let technologyArticles = [];
+let jqueryArticles = [];
+let nodejsArticles = [];
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+      console.log(response);
+      console.log(response.data);
+      response.data.articles.bootstrap.forEach((article) => {
+        articleCreator(bootstrapArticles, article);
+      });
+      bootstrapArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.javascript.forEach((article) => {
+        articleCreator(javascriptArticles, article);
+      });
+      javascriptArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.technology.forEach((article) => {
+        articleCreator(technologyArticles, article);
+      });
+      technologyArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.jquery.forEach((article) => {
+        articleCreator(jqueryArticles, article);
+      });
+      jqueryArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+      response.data.articles.topics[4].forEach((article) => {
+        articleCreator(nodejsArticles, article);
+      });
+      nodejsArticles.forEach((article)=>{
+          cardContainer.appendChild(article);
+      })
+
+})
+  .catch(error => {console.log('Error! : ' + error)});
+
+function articleCreator(array, obj){
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    let headlineDiv = document.createElement('div');
+    headlineDiv.classList.add('headline');
+    headlineDiv.textContent = obj.headline;
+    let authorDiv = document.createElement('div');
+    authorDiv.classList.add('author');
+    let imageDiv = document.createElement('div');
+    imageDiv.classList.add('img-container');
+    let img = document.createElement('img');
+    img.src = obj.authorPhoto;
+    let span = document.createElement('span');
+    span.textContent = obj.authorName;
+
+    imageDiv.appendChild(img);
+    authorDiv.appendChild(imageDiv);
+    authorDiv.appendChild(span);
+    cardDiv.appendChild(headlineDiv);
+    cardDiv.appendChild(authorDiv);
+
+    array.push(cardDiv);
+}
+
+//   function tabCreator(data){
+//       let tabDiv = document.createElement('div');
+//       tabDiv.classList.add('tab');
+//       tabDiv.textContent = data;
+//       topics.appendChild(tabDiv);
+//   }
